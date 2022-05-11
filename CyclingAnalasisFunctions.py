57fe0e440fac28acd53fe3d0d2d27a4cadc53254
Haggi1181 <yrrahtac2@gmail.com>
2022-05-11 20:14:25 +0100

    fixed up comments for functions and imporved CDA plotting

--- CyclingAnalasisFunctions.py
+++ CyclingAnalasisFunctions.py
@@ -10,6 +10,7 @@ import seaborn as sns
 import matplotlib.patheffects as path_effects
 import fluids
 from statistics import mean
+import sklearn as sk
 
 plt.close("all")
 
@@ -23,8 +24,16 @@ sns.set(rc={'figure.figsize':(15, 5)})
 def ReadData(DataPath):
     '''
     Function to Read Data from files downloaded from strava using the source plugin, fixes issues with spaces in names and empty watt values
-    Args: DataPath: File path for data to read
-    Returns: Pandas dataframe of data located at DataPath
+
+    Parameters
+    ----------
+    DataPath : String
+        File path for data to read
+
+    Returns
+    ---------- 
+    Data : Pandas dataframe 
+        data located at DataPath
     '''
 
 
@@ -37,11 +46,27 @@ def ReadData(DataPath):
 
     return Data
 
-def WPKFinder(DataPath, RiderMass, PrintData=False, GradeHillStart = 1, LengthOfClimeLowerBound = 10):
+def WPKFinder(DataPath, RiderMass, PrintData=False, GradeHillStart = 1, LengthOfClimeLowerBound = 10, NumberProcess = 10, DisplayFitLine = True):
     '''
     Function to find the Watts per Kg of a bike rider over a large period of time identifying periods of continuous grade over a threshhold value
-    Args: DataPath: File path of data(Note data must include the grade_smooth value in source which is not automatically ticked), RiderMass: mass of rider in kg, PrintData: if true prints raw values of each data point, GradeHillStart: Threshold value of which defines a hill over it data is recorded under it the hill is decided over(note this may cause climes with periods of changing gradients to become multiple climes), LengthOfClimeLowerBound: if the length of the clime is below this value in seconds it is removed from the data
+
+    Parameters
+    ----------
+    DataPath : String
+        File path of data(Note data must include the grade_smooth value in source which is not automatically ticked)
+    RiderMass : Float
+        mass of rider in kg, PrintData: if true prints raw values of each data point and annotates plot to allow for each point to be matched
+    GradeHillStart : Float
+        Threshold value of which defines a hill over it data is recorded under it the hill is decided over(note this may cause climes with periods of changing gradients to become multiple climes)
+    LengthOfClimeLowerBound : Integer
+        if the length of the clime is below this value in seconds it is removed from the data
+    NumberProcess : Integer
+        sets the number of performances to use in the plotting. selects the furthest from the origin to the closest. will display all if larger than number of climes
+    DisplayFitLine : Boolien
+        toggles the display of a line of best fit defined by a second order polyfit (note not allways desirable to be on as it causes strange fits for low amounts of data or large spreads in data)
+    ----------
     '''
+    plt.rcParams.update(plt.rcParamsDefault)
 
     RawData = ReadData(DataPath)
 
@@ -50,7 +75,6 @@ def WPKFinder(DataPath, RiderMass, PrintData=False, GradeHillStart = 1, LengthOf
     AccentAlt = [[]]
     Watts = [[]]
     Grade = [[]]
-
 
     AccentTimeTemp = []
     AccentAltTemp = []
@@ -64,7 +88,8 @@ def WPKFinder(DataPath, RiderMass, PrintData=False, GradeHillStart = 1, LengthOf
             WattsTemp.append(RawData["watts"][i])
             GradeTemp.append(RawData["grade_smooth"][i])
 
-        if(RawData["grade_smooth"][i+1])<GradeHillStart:
+
+        elif(RawData["grade_smooth"][i+1])<GradeHillStart and (RawData["grade_smooth"][i+2])<GradeHillStart and (RawData["grade_smooth"][i+3])<GradeHillStart and (RawData["grade_smooth"][i+4])<GradeHillStart and (RawData["grade_smooth"][i+5])<GradeHillStart and (RawData["grade_smooth"][i+6])<GradeHillStart and (RawData["grade_smooth"][i+7])<GradeHillStart and (RawData["grade_smooth"][i+8])<GradeHillStart and (RawData["grade_smooth"][i+9])<GradeHillStart and (RawData["grade_smooth"][i+10])<GradeHillStart:#lol this is the worst thing ive ever made i did a r/badprogramming meme
             AccentTime.append(AccentTimeTemp)
             AccentAlt.append(AccentAltTemp)
             Watts.append(WattsTemp)
@@ -75,7 +100,17 @@ def WPKFinder(DataPath, RiderMass, PrintData=False, GradeHillStart = 1, LengthOf
             WattsTemp = []
             GradeTemp = []
 
+        else:
+            AccentTimeTemp.append(RawData["time"][i])
+            AccentAltTemp.append(RawData["altitude"][i])
+            WattsTemp.append(RawData["watts"][i])
+            GradeTemp.append(RawData["grade_smooth"][i])
         i = i + 1
+
+    AccentTime.append(AccentTimeTemp)
+    AccentAlt.append(AccentAltTemp)
+    Watts.append(WattsTemp)
+    Grade.append(GradeTemp)
 
     i = 0
     while i != len(AccentTime):
@@ -101,7 +136,6 @@ def WPKFinder(DataPath, RiderMass, PrintData=False, GradeHillStart = 1, LengthOf
     aveWattsPK=[]
     aveGrade=[]
     ElapsedTime = []
-    RiderMass = 75
     j = 0
     while i != len(AccentTime):
         index.append(i)
@@ -111,21 +145,102 @@ def WPKFinder(DataPath, RiderMass, PrintData=False, GradeHillStart = 1, LengthOf
         i = i + 1
     i = 0
 
-    if PrintData == True:
-        while i != len(AccentTime):
-            print(index[i], aveWattsPK[i], aveGrade[i], ElapsedTime[i])
-            i = i + 1
-        i = 0
+
+    GoodnessIndex = []
+    tempgood = 0.0
+    while i != len(AccentTime):
+        tempgood = (aveWattsPK[i]/max(aveWattsPK))+(ElapsedTime[i]/max(ElapsedTime))
+        GoodnessIndex.append(tempgood)
+        i = i + 1
+    i = 0
+    #print(GoodnessIndex)
+
+    Rank = sp.argsort(GoodnessIndex)[::-1]
+    ProssessingWPK = []
+    ProssessingTime = []
+    plt.scatter(ElapsedTime,aveWattsPK, c="lightgray")
 
 
-    plt.scatter(ElapsedTime,aveWattsPK)
+    if NumberProcess > len(Rank):
+        NumberProcess = len(Rank)
 
+    #print(AccentTime[Rank[i]][0])
+    while i < NumberProcess:
+        if PrintData == True:
+            plt.scatter(ElapsedTime[Rank[i]],aveWattsPK[Rank[i]], c="dimgray", label = ("Starts at:",AccentTime[Rank[i]][0]))
+        else:
+            plt.scatter(ElapsedTime[Rank[i]],aveWattsPK[Rank[i]], c="k")
+        tempname = str(AccentTime[Rank[i]][0])
+        #print(tempname)
+        tempy = float(aveWattsPK[Rank[i]])
+        tempx = float(ElapsedTime[Rank[i]])
+        #print(tempx,tempy)
+        ProssessingWPK.append(aveWattsPK[Rank[i]])
+        ProssessingTime.append(ElapsedTime[Rank[i]])
+        if PrintData == True:
+            plt.annotate(tempname, (tempx, tempy))
+            grade = mean(Grade[Rank[i]])
+            tempname = round(float(tempname), 3)
+            tempy = round(float(tempy), 3)
+            tempx = round(float(tempx), 3)
+            grade = round(grade, 3)
+            print("Clime Starts at:", tempname, "s  WPK:", tempy, "w  For:", tempx,"s  On a average grade of:", grade, "%")
+        i = i + 1
+    #print(ProssessingWPK,ProssessingTime)
+    i = 0
+    temp = np.polyfit(ProssessingTime,ProssessingWPK,2)
+    linefitfunc = np.poly1d(temp)
+
+    linefitx=np.linspace(min(ElapsedTime),max(ElapsedTime),500)
+    linefity=linefitfunc(linefitx)
+    if DisplayFitLine == True:
+        plt.plot(linefitx,linefity, ls = '--', c="gray")
+    
+    
+    #plt.legend()
+    plt.xlabel("Time/s")
+    plt.ylabel("Watts per Kilo / w kg^-1")
+    plt.xlim(left=0)
+
+'''
+Failed exponential fit for above
+    popt, pcov = sp.optimize.curve_fit(fitEXPDecay, ProssessingTime, ProssessingWPK)
+    
+    linefitx=np.linspace(min(ProssessingTime),max(ProssessingTime),500)
+    linefity=fitEXPDecay(linefitx, *popt)
+
+    plt.plot(linefitx,linefity, ls = '--', c="gray")
+
+    def fitEXPDecay()
+
+    def fitEXPDecay(x, m, t, b):
+        return m * np.exp(-t * x) + b
+
+'''
 
 def calcCDA(Velocity, VelocityBef, Watts, AltitudeChange, Altitude, RiderMass, BikeMass, TimeScale = 1):
     '''
     Internal function that calculates the CDA of a bike rider for CDAPlot
-    Args: Velocity: Speed of bike rider, VelocityBef: Speed of bike rider at the position of Velocity - TimeScale, Watts: Power output of the bike rider, AltitudeChange: Change in altitude the bike rider has gone through through the timescale, Altitude: Current altitude of the bike rider, RiderMass: Mass of the bike rider, BikeMass: Mass of the bike riders bike, TimeScale: Length of iterations in seconds 
-    Returns: cda: the calculated CDA
+
+    Parameters
+    ----------
+    Velocity : Float
+        Speed of bike rider, 
+    VelocityBef : Float
+        Speed of bike rider at the position of Velocity - TimeScale, Watts: Power output of the bike rider, 
+    AltitudeChange : Float
+        Change in altitude the bike rider has gone through through the timescale, 
+    Altitude : Float
+        Current altitude of the bike rider, RiderMass: Mass of the bike rider, 
+    BikeMass : Float
+        Mass of the bike riders bike, 
+    TimeScale : Integer
+        Length of iterations in seconds 
+
+    Returns
+    ----------
+    cda : Float
+        the calculated CDA
     '''
     Riderm = RiderMass
     Bikem = BikeMass
@@ -153,7 +268,17 @@ def calcCDA(Velocity, VelocityBef, Watts, AltitudeChange, Altitude, RiderMass, B
 def CDAPlot(DataPath, RiderMass, BikeMass, TimeScale = 1):
     '''
     Plots CDA over time, resolution is TimeScale, data is presented as a box and whisker plot to identify average value, and plot of CDA/m^2 over time/s Function does not contain logic for non aero drag so be advised it is a overestimation
-    Args: DataPath: File path of raw data, RiderMass: Mass of bike rider, BikeMass: Mass of bike used, TimeScale: Length of iterations across time in seconds
+
+    Parameters
+    ----------
+    DataPath : String
+        File path of raw data
+    RiderMass : Float
+        Mass of bike rider
+    BikeMass : Float
+        Mass of bike used
+    TimeScale : Integer
+        Length of iterations across time in seconds
     '''
     RawData = ReadData(DataPath)
 
@@ -202,8 +327,29 @@ def CDAPlot(DataPath, RiderMass, BikeMass, TimeScale = 1):
 def calcDrag(Velocity, VelocityBef, Watts, AltitudeChange, Altitude, RiderMass, BikeMass, TimeScale = 1):
     '''
     Internal function that calculates the drag of a bike rider for CDAPlot
-    Args: Velocity: Speed of bike rider, VelocityBef: Speed of bike rider at the position of Velocity - TimeScale, Watts: Power output of the bike rider, AltitudeChange: Change in altitude the bike rider has gone through through the timescale, Altitude: Current altitude of the bike rider, RiderMass: Mass of the bike rider, BikeMass: Mass of the bike riders bike, TimeScale: Length of iterations in seconds 
-    Returns: Drag: the calculated drag
+    Parameters
+    ----------
+    Velocity : Float
+        Speed of bike rider
+    VelocityBef : Float
+        Speed of bike rider at the position of Velocity - TimeScale
+    Watts : Float
+        Power output of the bike rider
+    AltitudeChange : Float
+        Change in altitude the bike rider has gone through through the timescale
+    Altitude : Float
+        Current altitude of the bike rider
+    RiderMass : Float
+        Mass of the bike rider
+    BikeMass : Float
+        Mass of the bike riders bike
+    TimeScale : Integer
+        Length of iterations in seconds 
+
+    Returns
+    ----------
+    Drag : Float
+        the calculated drag
     '''
     Riderm = RiderMass
     Bikem = BikeMass
@@ -226,7 +372,17 @@ def calcDrag(Velocity, VelocityBef, Watts, AltitudeChange, Altitude, RiderMass, 
 def DragPlot(DataPath, RiderMass, BikeMass, TimeScale = 1):
     '''
     Plots drag over time, resolution is TimeScale, data is presented as a drag/w over time/s plot and a drag over speed plot with a function line for interpolation
-    Args: DataPath: File path of raw data, RiderMass: Mass of bike rider, BikeMass: Mass of bike used, TimeScale: Length of iterations across time in seconds
+    
+    Parameters
+    ----------
+    DataPath : String
+        File path of raw data
+    RiderMass : Float
+        Mass of bike rider
+    BikeMass : Float
+        Mass of bike used
+    TimeScale : Integer
+        Length of iterations across time in seconds
     '''
     RawData = ReadData(DataPath)
 
@@ -286,7 +442,13 @@ def add_median_labels(ax, precision='.3f'):
     function to add median data to box and whisker plots
     solution adapted from Christian Karcher's answer to:
     https://stackoverflow.com/questions/38649501/labeling-boxplot-in-seaborn-with-median-value
-    Args: ax: axis to add the label, precision: number of decimal places to add to plot
+
+    Parameters
+    ----------
+    ax : Matplotlib Axis
+        axis to add the label
+    precision : String
+        number of decimal places to add to plot
     '''
     lines = ax.get_lines()
     boxes = [c for c in ax.get_children() if type(c).__name__ == 'PathPatch']
